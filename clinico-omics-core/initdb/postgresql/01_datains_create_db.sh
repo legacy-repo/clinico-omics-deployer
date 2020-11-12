69a3c218364dbc38188fc68e649b5a755caeb2a5
YJC <yjcyxky@163.com>
2020-11-12 11:51:46 +0800

    1. Add openspecimen service;\n2. Improve clinico-omics-core service.

--- clinico-omics-core/initdb/postgresql/01_datains_create_db.sh
+++ clinico-omics-core/initdb/postgresql/01_datains_create_db.sh
@@ -8,3 +8,9 @@ echo "Creating database: datains"
 $POSTGRES <<EOSQL
 CREATE DATABASE datains OWNER datains;
 EOSQL
+
+# create database for superset
+echo "Creating database: tservice"
+$POSTGRES <<EOSQL
+CREATE DATABASE tservice OWNER tservice;
+EOSQL
